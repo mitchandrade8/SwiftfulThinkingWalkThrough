28279cdb16f2ddcf5ba711a8e542b6eab738afe6
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-07 01:47:08 -0600

    Initial Commit

---
+++ SwiftfulThinkingWalkThrough/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftfulThinkingWalkThrough
+//
+//  Created by Mitch Andrade on 6/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftfulThinkingWalkThrough/SwiftfulThinkingWalkThroughApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingWalkThroughApp.swift
+//  SwiftfulThinkingWalkThrough
+//
+//  Created by Mitch Andrade on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingWalkThroughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
