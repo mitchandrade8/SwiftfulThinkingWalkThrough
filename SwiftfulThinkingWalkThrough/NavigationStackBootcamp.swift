9014298b5ec5997ab914e6adb65406db45101b94
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-07 02:24:12 -0600

    - Differences between Navigation Stack and Navigation View

--- SwiftfulThinkingWalkThrough/NavigationStackBootcamp.swift
+++ SwiftfulThinkingWalkThrough/NavigationStackBootcamp.swift
@@ -8,19 +8,33 @@
 import SwiftUI
 
 struct NavigationStackBootcamp: View {
+    
+    let fruits = ["Apple", "Orange", "Banana"]
+    
     var body: some View {
-        NavigationView {
+        NavigationStack {
             ScrollView {
                 VStack(spacing: 40) {
+                    
+                    ForEach(fruits, id: \.self) { fruit in
+                        NavigationLink(value: fruit) {
+                            Text(fruit)
+                        }
+                    }
+                    
                     ForEach(0..<10) { x in
-                        NavigationLink(destination: {
-                            
-                        }, label: {
+                        NavigationLink(value: x) {
                             Text("Click me: \(x)")
-                        })
-                        .navigationTitle("Nav Bootcamp")
+                        }
                     }
                 }
+            }
+            .navigationTitle("Nav Bootcamp")
+            .navigationDestination(for: Int.self) { value in
+                MySecondScreen(value: value)
+            }
+            .navigationDestination(for: String.self) { value in
+                Text("ANOTHER SCREEN: \(value)")
             }
         }
     }
